a46a3d35e092300a6409796e525f7c37b20c188f
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-09-20 00:57:04 +0600

    part 2 react in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'Photoshop', price: '$90.99'},
+    {name: 'Illustrator', price: '$60.99'},
+    {name: 'PDF Reader', price: '$6.99'},
+    {name: 'Premiere El', price: '$249.99'},
+    {name: 'After Effects', price: '$249.99'},
+    {name: 'Lightroom', price: '$9.99'},
+    {name: 'InDesign', price: '$249.99'}
+    ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        products.map(product => <Product name = {product.name}></Product>)}
+      {/* <Product name="Laptop" price = '47000'></Product>
+      <Product name="IPhone" price = "120000"></Product>
+      <Product name="Watch" price = "3500"></Product> */}
     </div>
   );
+}
+
+function Product(props){
+  return(
+    <div className='product'>
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price} </p>
+    </div>
+  )
 }
 
 export default App;
